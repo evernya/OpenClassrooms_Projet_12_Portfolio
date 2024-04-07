06ce9396426a432460e49149a11f03e3331ae598
EmmaCYC <emmanuelle.cyc@live.fr>
2024-04-07 15:55:53 +0200

    Ancrage lien header

--- src/components/Navigation/Liens.jsx
+++ src/components/Navigation/Liens.jsx
@@ -7,13 +7,25 @@ import "../../style/style.css";
 
 
 export default function Navigation() {
+    //comportement
+    const handleNavigate = (elementId) => {
+        //Recherche de l'élément dans le document avec l'ID spécifié
+        const element = document.getElementById(elementId);
+
+        if (element) {
+            //Si l'élément existe défile jusqu'à cet élément
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+      };
+
+
     //affichage (render)
     return (
         <div className="nav-item__link--pf">
-            <NavLink to="/" className={({ isActive }) => `nav-item__link--pf-home ${isActive ? 'navlink-active' : ''}`}>Accueil</NavLink>
-            <NavLink to="/#PF" className={({ isActive }) => `nav-item__link--pf-pf ${isActive ? 'navlink-active' : ''}`}>Portfolio</NavLink>
-            <NavLink to="/about" className={({ isActive }) => `nav-item__link--pf-about ${isActive ? 'navlink-active' : ''}`}>À propos</NavLink>
-            <NavLink to="/#contact" className={({ isActive }) => `nav-item__link--pf-contact ${isActive ? 'navlink-active' : ''}`}>Contact</NavLink>
+            <NavLink to="/"  className={({ isActive }) => `nav-item__link--pf-home ${isActive ? 'link--pf-home-active' : ''}`}>Accueil</NavLink>
+            <NavLink to="/#PF" onClick={() => handleNavigate('PF')} className={({ isActive }) => `nav-item__link--pf-pf ${isActive ? 'link--pf-pf-active' : ''}`}>Portfolio</NavLink>
+            <NavLink to="/about" className={({ isActive }) => `nav-item__link--pf-about ${isActive ? 'link--pf-about-active' : ''}`}>À propos</NavLink>
+            <NavLink to="/#contact" onClick={() => handleNavigate('contact')} className={({ isActive }) => `nav-item__link--pf-contact ${isActive ? 'link--pf-contact-active' : ''}`}>Contact</NavLink>
         </div>
     );
 }
