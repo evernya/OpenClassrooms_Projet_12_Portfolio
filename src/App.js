b189917915f26a4aff23cedca583012fd46045e2
EmmaCYC <emmanuelle.cyc@live.fr>
2024-04-18 14:29:48 +0200

    Ajout fonction pour scroll en haut de la page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+//IMPORT
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+//PAGES
+import Home from "../src/pages/Home/Home";
+import Projet from "../src/pages/Projet/Projet";
+import About from "../src/pages/About/About";
+import Error from "../src/pages/Error/Error";
+
+//LAYOUTS
+import Header from "../src/layouts/Header";
+import Footer from "../src/layouts/Footer";
+
+//COMPONENTS
+
+//ASSETS
+
+//CSS
+import "./style/style.css";
+import ScrollToTop from "./components/Navigation/ScrollToUp";
 
 function App() {
+  //state (état, données)
+
+  //comportement
+
+  //affichage (render)
   return (
-    <div className="App">
-     Hello World !
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projet/:projetId" element={<Projet />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navigation/Liens.jsx
+++ src/components/Navigation/Liens.jsx
@@ -18,13 +18,20 @@ export default function Navigation() {
         }
       };
 
+      const scrollToTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      };
+
 
     //affichage (render)
     return (
         <div className="nav-item__link--pf">
-            <NavLink to="/"  className={({ isActive }) => `nav-item__link--pf-home ${isActive ? 'link--pf-home-active' : ''}`}>Accueil</NavLink>
+            <NavLink to="/" onClick={() => scrollToTop} className={({ isActive }) => `nav-item__link--pf-home ${isActive ? 'link--pf-home-active' : ''}`}>Accueil</NavLink>
             <NavLink to="/#PF" onClick={() => handleNavigate('PF')} className={({ isActive }) => `nav-item__link--pf-pf ${isActive ? 'link--pf-pf-active' : ''}`}>Portfolio</NavLink>
-            <NavLink to="/about" className={({ isActive }) => `nav-item__link--pf-about ${isActive ? 'link--pf-about-active' : ''}`}>À propos</NavLink>
+            <NavLink to="/about" onClick={() => scrollToTop} className={({ isActive }) => `nav-item__link--pf-about ${isActive ? 'link--pf-about-active' : ''}`}>À propos</NavLink>
             <NavLink to="/#contact" onClick={() => handleNavigate('contact')} className={({ isActive }) => `nav-item__link--pf-contact ${isActive ? 'link--pf-contact-active' : ''}`}>Contact</NavLink>
         </div>
     );
